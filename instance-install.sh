93e668b162e8b243c7f5498af7050973e07a4421
Joffrey Darcq <j-off@live.fr>
2017-10-03 11:45:33 +0200

    Update instance-install.sh

--- instance-install.sh
+++ instance-install.sh
@@ -79,7 +79,7 @@ After=syslog.target network.target
 [Service]
 Type=forking
 WorkingDirectory=/home/seafile/%i
-ExecStart=/usr/bin/seafile-admin start # --fastcgi
+ExecStart=/usr/bin/seafile-admin start
 ExecStop=/usr/bin/seafile-admin stop
 PIDFile=/home/seafile/%i/seafile-data/pids/seaf-server.pid
 User=seafile
